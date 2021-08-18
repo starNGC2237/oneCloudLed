45332a022111e1c4921974b943469ff6b6e16a87
指路天极 <2236992924@qq.com>
2021-08-18 06:41:17 +0000

    add led/ledflash.sh.

---
+++ led/ledflash.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ledon blue
+sleep 1s
+ledoff blue
+sleep 1s
+ledon red
+sleep 1s
+ledoff red
+sleep 1s
+ledon green 
+sleep 1s
+ledoff green
+sleep 1s
+ledon red
+ledon green
+sleep 1s
+ledoff red
+ledoff green
+sleep 1s
+ledon red
+ledon blue
+sleep 1s
+ledoff red
+ledoff blue
+sleep 1s
+ledon green
+ledon blue
+sleep 1s
+ledoff green
+ledoff blue
+sleep 1s
+ledon red
+ledon blue
+ledon green
+sleep 1s
+ledoff red
+ledoff blue
+ledoff green
+
